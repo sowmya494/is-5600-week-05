7041417fe83c54183e19f3dfe3482b8378ccf0b2
sowmya494 <sowmyarao.darineni@slu.edu>
2025-11-24 00:21:50 +0000

    final lab 5

--- api.js
+++ api.js
@@ -1,85 +1,42 @@
-const path = require('path')
 const Products = require('./products')
-const autoCatch = require('./lib/auto-catch')
 
-/**
- * Handle the root route
- * @param {object} req
- * @param {object} res
-*/
-function handleRoot(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
-/**
- * List all products
- * @param {object} req
- * @param {object} res
- */
-async function listProducts(req, res) {
-  // Extract the limit and offset query parameters
-  const { offset = 0, limit = 25, tag } = req.query
-  // Pass the limit and offset to the Products service
-  res.json(await Products.list({
+async function listProducts(req, res, next) {
+  const { offset = 0, limit = 25, tag } = req.query;
+
+  const products = await Products.list({
     offset: Number(offset),
     limit: Number(limit),
     tag
-  }))
+  });
+
+  res.json(products);
 }
 
 
-/**
- * Get a single product
- * @param {object} req
- * @param {object} res
- */
 async function getProduct(req, res, next) {
-  const { id } = req.params
-
-  const product = await Products.get(id)
-  if (!product) {
-    return next()
-  }
-
-  return res.json(product)
+  const product = await Products.get(req.params.id)
+  res.json(product)
 }
 
-/**
- * Create a product
- * @param {object} req 
- * @param {object} res 
- */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
-}
-
-/**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
- */
 async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
+  const product = await Products.edit(req.params.id, req.body)
+  res.json(product)
 }
 
-/**
- * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- */
 async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
 }
 
-module.exports = autoCatch({
-  handleRoot,
+module.exports = {
+  createProduct,
   listProducts,
   getProduct,
-  createProduct,
   editProduct,
   deleteProduct
-});
+}
--- products.js
+++ products.js
@@ -1,49 +1,85 @@
-const fs = require('fs').promises
-const path = require('path')
+// products.js
+const cuid = require('cuid')
+const db = require('./db')
 
-const productsFile = path.join(__dirname, 'data/full-products.json')
+// Define our Product Model
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: { type: String },
+  alt_description: { type: String },
+  likes: { type: Number, required: true },
+  urls: {
+    regular: { type: String, required: true },
+    small: { type: String, required: true },
+    thumb: { type: String, required: true },
+  },
+  links: {
+    self: { type: String, required: true },
+    html: { type: String, required: true },
+  },
+  user: {
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    portfolio_url: { type: String },
+    username: { type: String, required: true },
+  },
+  tags: [{
+    title: { type: String, required: true },
+  }], 
+})
+
+/**
+ * Create new product
+ */
+async function create(fields) {
+  return await new Product(fields).save()
+}
 
 /**
  * List products
- * @param {*} options 
- * @returns 
  */
 async function list(options = {}) {
+  const { offset = 0, limit = 25, tag } = options
 
-  const { offset = 0, limit = 25, tag } = options;
+  const query = tag ? {
+    tags: { $elemMatch: { title: tag } }
+  } : {}
 
-  const data = await fs.readFile(productsFile)
-  return JSON.parse(data)
-    .filter(product => {
-      if (!tag) {
-        return product
-      }
-
-      return product.tags.find(({ title }) => title == tag)
-    })
-    .slice(offset, offset + limit) // Slice the products
+  return await Product.find(query)
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(limit)
 }
 
 /**
- * Get a single product
- * @param {string} id
- * @returns {Promise<object>}
+ * Get one product
  */
-async function get(id) {
-  const products = JSON.parse(await fs.readFile(productsFile))
+async function get(_id) {
+  return await Product.findById(_id)
+}
 
-  // Loop through the products and return the product with the matching id
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === id) {
-      return products[i]
-    }
-  }
+/**
+ * Edit product
+ */
+async function edit(_id, change) {
+  const product = await get(_id)
+  Object.keys(change).forEach(key => product[key] = change[key])
+  await product.save()
+  return product
+}
 
-  // If no product is found, return null
-  return null;
+/**
+ * Delete product
+ */
+async function destroy(_id) {
+  return await Product.deleteOne({ _id })
 }
 
 module.exports = {
+  create,
   list,
-  get
+  get,
+  edit,
+  destroy
 }
